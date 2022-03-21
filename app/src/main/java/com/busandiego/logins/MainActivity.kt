2b3168fc602747bebc19a1ab73bc8ab0665a6ad4
suin lee <silee@weedscomm.com>
2022-03-21 23:47:56 +0900

    implement, manifest edit

--- app/src/main/java/com/busandiego/logins/MainActivity.kt
+++ app/src/main/java/com/busandiego/logins/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.busandiego.logins
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+
+
+
+
 }
