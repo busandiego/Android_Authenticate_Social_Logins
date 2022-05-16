04fd378ac285dd317c3da726870740e09db87c63
suin lee <silee@weedscomm.com>
2022-05-16 22:48:41 +0900

    delete token 메소드 테스트

--- app/src/main/java/com/busandiego/logins/MainActivity.kt
+++ app/src/main/java/com/busandiego/logins/MainActivity.kt
@@ -32,6 +32,14 @@ class MainActivity : AppCompatActivity() {
                 this, callback = naverLoginCallback
             )
         }
+
+        val naverLogoutButton = findViewById<Button>(R.id.naver_logout_button)
+        naverLogoutButton.setOnClickListener {
+            // NaverIdLoginSDK.logout()
+            // Log.d(TAG, "NidOAuthLoginState.OK.equals(NaverIdLoginSDK.getState()) >>>>>>> ${NidOAuthLoginState.OK.equals(NaverIdLoginSDK.getState())}")
+            // 처리
+
+        }
     }
 
     /**
@@ -82,6 +90,30 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+    }
+
+    // 토큰 날려버리기
+    fun deleteToken(){
+        NidOAuthLogin().callDeleteTokenApi(this, object : OAuthLoginCallback {
+            override fun onSuccess() {
+                // 서버에서 토큰 삭제에 성공한 상태입니다.
+                // updateView()
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(this@MainActivity, "errorCode: $errorCode, errorDesc: $errorDescription", Toast.LENGTH_SHORT).show()
+                // updateView()
+            }
+            override fun onError(errorCode: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
+            }
+        })
 
     }
 }
