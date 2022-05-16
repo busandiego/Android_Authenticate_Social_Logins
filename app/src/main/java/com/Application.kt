e8ebaaae7ab002225d5865fc193eb5673da27464
suin lee <silee@weedscomm.com>
2022-05-16 22:01:55 +0900

    string 추가

--- app/src/main/java/com/Application.kt
+++ app/src/main/java/com/Application.kt
@@ -9,6 +9,9 @@ class Application: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // OAUTH_CLIENT_ID: 애플리케이션 등록 후 발급받은 클라이언트 아이디
+        // OAUTH_CLIENT_SECRET: 애플리케이션 등록 후 발급받은 클라이언트 시크릿
+        // OAUTH_CLIENT_NAME: 네이버 앱의 로그인 화면에 표시할 애플리케이션 이름, 모바일 웹의 로그인 화면을 사용할 때는 서버에 저장된 애플리케이션 이름이 표시됩니다.
         NaverIdLoginSDK.initialize(this, "", "", "")
     }
 
