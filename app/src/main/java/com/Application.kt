cde28dce12bc14d9f485b9cf34f20a6f7b40c512
suin lee <silee@weedscomm.com>
2022-05-16 22:23:31 +0900

    종속성 오류 수정

--- app/src/main/java/com/Application.kt
+++ app/src/main/java/com/Application.kt
@@ -1,6 +1,7 @@
 package com
 
 import android.app.Application
+import com.busandiego.logins.R
 import com.navercorp.nid.NaverIdLoginSDK
 
 class Application: Application() {
@@ -12,7 +13,7 @@ class Application: Application() {
         // OAUTH_CLIENT_ID: 애플리케이션 등록 후 발급받은 클라이언트 아이디
         // OAUTH_CLIENT_SECRET: 애플리케이션 등록 후 발급받은 클라이언트 시크릿
         // OAUTH_CLIENT_NAME: 네이버 앱의 로그인 화면에 표시할 애플리케이션 이름, 모바일 웹의 로그인 화면을 사용할 때는 서버에 저장된 애플리케이션 이름이 표시됩니다.
-        NaverIdLoginSDK.initialize(this, "", "", "")
+        NaverIdLoginSDK.initialize(this, getString(R.string.naver_client_id), getString(R.string.naver_client_secert), getString(R.string.naver_client_name))
     }
 
 }
--- app/src/main/java/com/busandiego/logins/MainActivity.kt
+++ app/src/main/java/com/busandiego/logins/MainActivity.kt
@@ -6,18 +6,52 @@ import android.content.pm.Signature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
 
 class MainActivity : AppCompatActivity() {
-    private  val TAG = "MainActivity"
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val naverButton = findViewById<Button>(R.id.naver_button)
+        naverButton.setOnClickListener {
+            NaverIdLoginSDK.authenticate(
+                this, callback = naverLoginCallback
+            )
+        }
+
+
     }
 
+    /**
+     * OAuthLoginCallback을 authenticate() 메서드 호출 시 파라미터로 전달하거나 NidOAuthLoginButton 객체에 등록하면 인증이 종료되는 것을 확인할 수 있습니다.
+     */
+
+    val naverLoginCallback = object : OAuthLoginCallback {
+
+        // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+        override fun onSuccess() {
+           Log.d(TAG, "NaverIdLoginSDK.getAccessToken() >>> ${NaverIdLoginSDK.getAccessToken()}")
+           Log.d(TAG, "NaverIdLoginSDK.getRefreshToken() >>> ${NaverIdLoginSDK.getRefreshToken()}")
+           Log.d(TAG, "NaverIdLoginSDK.getExpiresAt().toString() >>> ${NaverIdLoginSDK.getExpiresAt().toString()}")
+           Log.d(TAG, "NaverIdLoginSDK.getTokenType() >>> ${NaverIdLoginSDK.getTokenType()}")
+           Log.d(TAG, "NaverIdLoginSDK.getState().toString() >>> ${NaverIdLoginSDK.getState().toString()}")
+        }
+
+        override fun onError(errorCode: Int, message: String) {
+            val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+            val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+            Toast.makeText(applicationContext,"errorCode:$errorCode, errorDesc:$errorDescription",Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onFailure(errorCode: Int, message: String) {
+            onFailure(errorCode, message)
+        }
 
 
-
-
-
-
+    }
 }
